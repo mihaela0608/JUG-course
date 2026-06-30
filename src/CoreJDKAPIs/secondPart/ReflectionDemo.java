3aefebc087d8762222dedd8cd47ff616ab374601
mihaela0608 <mihaelabataklieva2006@gmail.com>
2026-06-30 17:57:36 +0300

    Adding reflection and regex exercise

---
+++ src/CoreJDKAPIs/secondPart/ReflectionDemo.java
@@ -0,0 +1,20 @@
+package CoreJDKAPIs.secondPart;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ReflectionDemo {
+    public static void main(String[] args) {
+        Person person = new Person("Iva", 15);
+        reflection(person);
+    }
+    
+    public static void reflection(Object object){
+        Stream<Field> fields = Arrays.stream(object.getClass().getDeclaredFields());
+        Stream<Constructor<?>> constructors = Arrays.stream(object.getClass().getConstructors());
+        fields.forEach(System.out::println);
+        constructors.forEach(System.out::println);
+    }
+}
---
+++ src/CoreJDKAPIs/secondPart/RegexDemo.java
@@ -0,0 +1,13 @@
+package CoreJDKAPIs.secondPart;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^\\d+A");
+
+        Matcher matcher = pattern.matcher("3A");
+        System.out.println(matcher.find());
+    }
+}
